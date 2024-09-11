fd1a944a0220b4d122092b8bbd0fe3834bb63581
bayubiantara0 <bayubiantara0@gmail.com>
2024-09-11 22:58:39 +0700

    feat: Implement edit method for task management

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Edit Task</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Edit Task</h1>
+        <form action="{{ route('tasks.update', $task->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" class="form-control" value="{{ $task->name }}" required>
+            </div>
+            <div class="form-group">
+                <label for="description">Description:</label>
+                <textarea id="description" name="description" class="form-control">{{ $task->description }}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="date">Date:</label>
+                <input type="date" id="date" name="date" class="form-control" value="{{ $task->date ? $task->date->format('Y-m-d') : '' }}">
+            </div>
+            <button type="submit" class="btn btn-primary">Update</button>
+        </form>
+    </div>
+</body>
+
+</html>
